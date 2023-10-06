8f709a330161f769794ab63a77b944cb52417d4e
Oleg Marichev <sunnydrake7@gmail.com>
2023-10-06 18:07:51 +0300

    Initial commit

---
+++ packages/plg_auth_phonex-1.0.0/src/Extension/Phonex.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Noting original copyright ownership prior to update to Joomla 4
+ * package		plg_auth_email
+ * copyright	Copyright (C) 2005 - 2011 Michael Richey. All rights reserved.
+ * Copyright (C) 2023 Oleg Marychev
+ * license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Plugin\Authentication\Phonex\Extension;
+
+use Joomla\CMS\Authentication\Authentication;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseAwareTrait;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * @package Joomla\Plugin\Authentication\Phonex\Extension
+ * @author Merrill Squiers
+ * @author Oleg Marychev
+ * @since  Joomla 4.0
+ * @version 1.0.0
+ */
+final class Phonex extends CMSPlugin
+{
+    // NOTE: Cannot use subscriberinterface because Authentication.php explicitly requires 
+    // onUserAuthenticate method as of Joomla 4.3.1
+
+
+
+    use DatabaseAwareTrait;
+
+    // The following properties are initialized by CMSPlugin::__construct()
+    protected $db;
+    protected $app;
+    protected $autoloadLanguage = true;
+
+    private $_paj;
+
+    public function __construct(&$subject, $config = [])
+    {
+        parent::__construct($subject, $config);
+        $paj = PluginHelper::getPlugin('authentication', 'joomla');
+        $this->_paj = new \Joomla\Plugin\Authentication\Joomla\Extension\Joomla($subject, (array)$paj);
+    }
+
+    /**
+     * This method should handle any authentication and report back to the subject
+     */
+    function onUserAuthenticate(&$credentials, $options, &$response)
+    {
+        $query = $this->db->getQuery(true);
+
+        $username = $this->app->input->post->get('username', false, 'RAW');
+//        $query->select('id, username, password')
+//            ->from('#__users')
+//            ->where('block = 0')
+           // ->where('UPPER(email) = UPPER(' . $this->db->Quote($username) . ')');
+        $query->select('u.id as id, username, password')
+        ->from('#__users as u')
+	        ->join("RIGHT", "#__fields_values AS fv ON u.id = fv.item_id and fv.value=" . $this->db->Quote($username))
+	        ->join("RIGHT", "#__fields as f ON fv.field_id = f.id and f.name='phone'")
+			->where('u.block = 0');
+        $this->db->setQuery($query);
+        $result = $this->db->loadObject();
+
+        if ($result) {
+            // why mess with re-creating authentication - just use the system.
+            $credentials['username'] = $result->username;
+            $this->_paj->setDatabase($this->db);
+            $this->_paj->setApplication($this->app);
+            $this->_paj->onUserAuthenticate($credentials, $options, $response);
+        } else {
+            $response->status = Authentication::STATUS_FAILURE;
+            $response->error_message = Text::_('JGLOBAL_AUTH_INVALID_PASS');
+        }
+    }
+}
---
+++ packages/plg_system_phonex-1.0.0/phonex.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Original author's header:
+ * version		$Id: email.php 20196 2011-03-04 02:40:25Z mrichey $
+ * package		plg_auth_email
+ * copyright	Copyright (C) 2005 - 2011 Michael Richey. All rights reserved.
+ * Copyright (C) 2023 Oleg Marychev
+ * license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Event\SubscriberInterface;
+
+/**
+ * @package plgSystemPhonex
+ * @author Merrill Squiers / Oleg Marychev
+ * @since  Joomla 4.0
+ * @version 1.0.0
+ */
+
+class plgSystemPhonex extends CMSPlugin implements SubscriberInterface
+{
+    // The following properties are initialized by CMSPlugin::__construct()
+    protected $db;
+    protected $app;
+    protected $autoloadLanguage = true;
+
+    public static function getSubscribedEvents(): array { 
+        return [
+            'onAfterRoute' => 'handleResetConfirm',
+        ];
+    }
+
+	function handleResetConfirm()
+	{
+            $app = $this->app;
+            if($app->getName() === 'administrator') return;
+            $component = $app->input->getCmd('option');
+            if($component != 'com_users') return;
+            $task = $app->input->getCmd('task');
+            if($task != 'reset.confirm') return;
+            
+            // ok, at this point we know that the form has been submitted.
+            $jform = $app->input->get('jform',array(),'array');
+            if(count($jform) && preg_match('/@/',$jform['username'])) {
+                $db = $this->db;
+                $query = $db->getQuery(true);
+//                $query->select('username')
+//                    ->from('#__users')
+//                    ->where('UPPER(email) = UPPER('.$db->quote($jform['username']).')')
+//                    ->where('block = 0');
+	            $query->select('u.id as id, username, password')
+		            ->from('#__users as u')
+		            ->join("RIGHT", "#__fields_values AS fv ON u.id = fv.item_id and fv.value=" . $db->quote($jform['username']))
+		            ->join("RIGHT", "#__fields as f ON fv.field_id = f.id and f.name='phone'")
+		            ->where('u.block = 0');
+                $db->setQuery($query);
+                $username = $db->loadResult();
+                if( $username !== null ) {
+                    $jform['username']=$username;
+                    $app->input->set('jform',$jform);
+                }
+            }
+        }
+}
